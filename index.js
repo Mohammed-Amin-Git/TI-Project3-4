3985765319dcfa8157fc0e52911fb0d4f1bd5147
Mohammed-Amin-dev <amin.developer@hotmail.com>
2024-03-12 23:07:26 +0100

    Added enviroment variables to the server and SSH Tunnel for the MySQL database connection

--- index.js
+++ index.js
@@ -1,15 +1,67 @@
 import { ReadlineParser } from '@serialport/parser-readline';
+import { createTunnel } from 'tunnel-ssh';
 import { SerialPort } from 'serialport';
 import { WebSocketServer } from 'ws';
+import mysql from 'mysql2/promise';
 import express from 'express';
+import 'dotenv/config';
 
 const app = express();
 const wss = new WebSocketServer({ port: 8080 });
 
 app.use(express.static('public'));
 
-const port = new SerialPort({ path: "COM5", baudRate: 9600 });
+const port = new SerialPort({ path: process.env.SERIAL_PORT, baudRate: 9600 });
 const parser = port.pipe(new ReadlineParser());
+
+const tunnelOptions = {
+  autoClose:true
+}
+
+const serverOptions = {
+  port: process.env.DB_PORT
+}
+
+const sshOptions = {
+  host: process.env.DB_SSH_HOST,
+  port: 22,
+  username: process.env.DB_SSH_USER,
+  password: process.env.DB_SSH_PASSWORD
+}
+
+const forwardOptions = {
+  srcAddr: '127.0.0.1',
+  srcPort: process.env.DB_SSH_PORT,
+  dstAddr: '127.0.0.1',
+  dstPort: process.env.DB_SSH_PORT
+}
+
+let [server, conn] = await createTunnel(tunnelOptions, serverOptions, sshOptions, forwardOptions);
+
+server.on('connection', (connection) => {
+  console.log("SSH Tunnel succesful");
+});
+
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
+// BEGIN TESTING: MySQL data retrieval
+
+// db.query("SELECT * FROM accounts").then(([rows, fields]) => {
+//   console.log(rows);
+// })
+
+(async () => {
+  let data = await db.query("SELECT * FROM accounts");
+  console.log(data[0]); 
+})();
+
+// END TESTING
 
 // States: NULL, SCAN_CARD, PINCODE, OPTIONS
 let CLIENT_STATE = "NULL";
---
+++ ssh_tunnel.sh
@@ -0,0 +1,1 @@
+ssh -N -p 22 bank@145.24.223.56 -L 33306:localhost:8000
