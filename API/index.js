21a48a6efba225a8141cb5c962a758aadc30c9ca
Mohammed-Amin-Git <bank@1079623-ubuntu-2024-juli.datalabrotterdam.nl>
2024-04-14 13:20:28 +0000

    Updated the api so that it runs correctely on the global server

--- API/index.js
+++ API/index.js
@@ -1,12 +1,11 @@
-const express = require('express');
-const cors = require('cors');
+import { validateRequest, getCustomerByIBANandUID, getCustomerInfo, updateAttemptsRemaining, blockCardOfCustomer } from './modules/validationFunctions.js';
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
 const app = express();
 
 app.use(cors());
-
-app.listen(8080, () => {
-	console.log("Creating API Server: http://145.24.223.56:8080");
-});
 
 app.get("/api/noob/health", (req, res) => {
 	res.json({
@@ -14,12 +13,51 @@ app.get("/api/noob/health", (req, res) => {
 	});
 });
 
-app.get("/api/accountinfo", (req, res) => {
-	let iban = req.query.iban;
-	let pincode = req.query.iban;
-	let uid = req.query.iban;
+let customer_attempts_remaining = {};
 
-	if(!iban || !pincode || !uid) {
-		res.status(400).json();
+app.get("/api/accountinfo", async (req, res) => {
+	let iban = req.query.iban;
+	let pincode = req.query.pincode;
+	let uid = req.query.uid;
+
+	try {
+		if(!validateRequest(iban, pincode, uid)) {
+			res.status(400).json();
+		} else {
+			// Checking if the customer exists
+			const customer = await getCustomerByIBANandUID(iban, uid);
+			if(!customer) {
+				return res.status(404).json();
+			}
+	
+			// Checking if the card of the customer is blocked
+			if(customer.Card_blocked) {
+				return res.status(403).json();
+			}
+	
+			// Checking if the pincode of the customer is correct
+			const customerInfo = await getCustomerInfo(customer.Customer_ID, pincode);
+			if(!customerInfo) {
+				customer_attempts_remaining = updateAttemptsRemaining(customer_attempts_remaining, customer.Customer_ID);
+				
+				if(customer_attempts_remaining[customer.Customer_ID] <= 0) {
+					blockCardOfCustomer(customer.Customer_ID);
+					return res.status(403).json();
+				} else {
+					return res.status(401).json({
+						attempts_remaining: customer_attempts_remaining[customer.Customer_ID]
+					});
+				}
+			}
+	
+			// Return user data because authentication was successful
+			customer_attempts_remaining[customer.Customer_ID] = 3;
+			res.status(200).json(customerInfo);
+		}
+	} catch(err) {
+		res.status(500).json();
+		throw err;
 	}
 });
+
+app.listen(process.env.PORT, () => console.log(`Creating API Server: http://145.24.223.56:${process.env.PORT}`));
---
+++ API/modules/validationFunctions.js
@@ -0,0 +1,49 @@
+import mysql from 'mysql2/promise';
+import 'dotenv/config';
+
+// MySQL Config
+const db = mysql.createPool({
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	user: process.env.DB_USERNAME,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DATABASE
+});
+
+
+export function validateRequest(iban, pincode, uid) {
+	if(!iban || !pincode || !uid) {
+		return false;
+	}
+	
+	return true;
+}
+
+export async function getCustomerByIBANandUID(iban, uid) {
+	const sql = "SELECT Customer_ID, Card_blocked FROM Customer WHERE IBAN = ? AND Pass_number = ?";
+	const [rows, fields] = await db.query(sql, [iban, uid]);
+	return rows.length > 0 ? rows[0] : null;
+}
+
+export async function getCustomerInfo(customer_id, pincode) {
+	const sql = "SELECT Firstname, Lastname, Balance FROM Customer WHERE Customer_ID = ? AND Pincode = ?";
+	const [rows, fields] = await db.query(sql, [customer_id, pincode]);
+	return rows.length > 0 ? rows[0] : null;
+}
+
+export function blockCardOfCustomer(customer_id) {
+	const sql = "UPDATE Customer SET Card_blocked = 1 WHERE Customer_ID = ?";
+	db.query(sql, [customer_id]);
+}
+
+export function updateAttemptsRemaining(attempts, customer_id) {
+	if(Object.keys(attempts).includes(customer_id.toString())) {
+		if(attempts[customer_id] > 0) {
+			attempts[customer_id] = attempts[customer_id] - 1;
+		}
+	} else {
+		attempts[customer_id] = 2;
+	}
+
+	return attempts;
+}
