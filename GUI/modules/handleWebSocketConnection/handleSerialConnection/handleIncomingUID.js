a06877cb22eeb5efafa71d707819f66c3249ff18
Mohammed-Amin-dev <amin.developer@hotmail.com>
2024-05-04 21:18:13 +0200

    Modified the code so that it integrates with other banks via the NOOB server. User of another bank now can scan their card and login to the options menu

--- GUI/modules/handleWebSocketConnection/handleSerialConnection/handleIncomingUID.js
+++ GUI/modules/handleWebSocketConnection/handleSerialConnection/handleIncomingUID.js
@@ -1,8 +1,8 @@
 import { db } from "../databaseConnectionModule/createDBConnectionViaSSH.js";
-import { GLOBAL } from "../../handleWebSocketConnection.js";
+import { GLOBAL, NOOBRequest } from "../../handleWebSocketConnection.js";
 
 // TODO: function handleIncomingUID should take a third parameter: IBAN
-export function handleIncomingUID(ws, uid, iban) {
+export async function handleIncomingUID(ws, uid, iban) {
     if(uid == "" || iban == "" || !uid.match(/[0-9A-F]{8}/) || !iban.match(/[A-Z]{2}[0-9]{2}[A-Z]{4}[0-9]{10}/)) {
         ws.send(JSON.stringify({
             "type": "ERROR",
@@ -12,23 +12,45 @@ export function handleIncomingUID(ws, uid, iban) {
     }
     
     // Looking if the scanned UID is in the database
-    db.query("SELECT Customer_ID, Card_blocked FROM Customer WHERE Pass_number = ? AND IBAN = ?", [uid, iban]).then(([rows, fields]) => {
-        if(rows.length == 0) {
-          
-          
-            // TODO: Check if the IBAN is available at another bank, then set NOOB flag
-          ws.send(JSON.stringify({
-            "type": "ERROR",
-            "data": "SCAN_CARD_NOT_EXIST"
-          }));
-          GLOBAL.CLIENT_STATE = "SCAN_CARD";
+    db.query("SELECT Customer_ID, Card_blocked FROM Customer WHERE Pass_number = ? AND IBAN = ?", [uid, iban]).then(async ([rows, fields]) => {
+        // TODO: Remove iban.includes, this is only for testing purposes
+        if(rows.length == 0 || iban.includes("TESB")) {
+          // TODO: Check if the IBAN is available at another bank, then set NOOB flag
+          const response = await NOOBRequest("POST", "accountinfo", iban, {"target": iban, "uid": uid});
+          console.log(response);
+          switch(response.status_code) {
+            case 400:
+                ws.send(JSON.stringify({
+                    "type": "ERROR",
+                    "data": "INVALID_CARD"
+                }));
+                break;
+            case 404:
+                ws.send(JSON.stringify({
+                    "type": "ERROR",
+                    "data": "SCAN_CARD_NOT_EXIST"
+                }));
+            case 403:
+                ws.send(JSON.stringify({
+                    "type": "ERROR",
+                    "data": "CARD_BLOCKED"
+                }));
+            case 401:
+                ws.send(JSON.stringify({
+                    "type": "REDIRECT",
+                    "data": "PINCODE"
+                }));
+    
+                GLOBAL.NOOB_FLAG = true;
+                GLOBAL.CLIENT_STATE = "PINCODE";
+          }
 
         } else {
             // Checking if the card is blocked
             if(rows[0].Card_blocked) {
                 ws.send(JSON.stringify({
-                "type": "ERROR",
-                "data": "CARD_BLOCKED"
+                    "type": "ERROR",
+                    "data": "CARD_BLOCKED"
                 }));
             } else {
                 // SCAN_CARD success
